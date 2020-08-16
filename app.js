4f51e3856befe661a82bf0e37d5bf3984ee2abe7
ali salemmi <alisalemmi@outlook.com>
2020-08-16 17:53:36 +0430

    روی سرور mongodb طراحی یک کد برای تست

--- app.js
+++ app.js
@@ -1,9 +1,48 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config({
+  path: './config.env'
+});
+
+mongoose.connect(
+  process.env.MONGODB,
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+  },
+  () => {
+    console.log('DB connection successfull :)');
+  }
+);
+
+const TestSchema = new mongoose.Schema({
+  name: String,
+  age: Number
+});
+
+const Test = mongoose.model('Test', TestSchema);
 
 const app = express();
 
-app.get('/:id', (req, res) => {
+app.get('/', async (req, res) => {
+  const tour = await Test.find();
+
+  res.status(200).json(tour);
+});
+
+app.get('/add/:name/:age', async (req, res) => {
+  const t = new Test({
+    name: req.params.name,
+    age: req.params.age
+  });
+
+  await t.save();
   res.status(200).json({ message: 'hello', params: req.params });
 });
 
-app.listen(3000, () => console.log('App is running on port 3000 ...'));
+app.listen(process.env.PORT, () =>
+  console.log(`App is running on port ${process.env.PORT} ...`)
+);
